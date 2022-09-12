5b00fd00e3c92aa272ee24656075fad442ba8636
JC Tula <jc_tula@outlook.com>
2022-09-12 08:49:45 -0600

    Estructura, capa entidades, dal y pruebas unitarias

---
+++ DeptosES.AccesoADatos/BDContexto.cs
@@ -0,0 +1,23 @@
+﻿using DeptosES.EntidadesDeNegocio;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeptosES.AccesoADatos
+{
+    public class BDContexto : DbContext
+    {
+        public DbSet<Rol> Rol { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Departamento> Departamento { get; set; }
+        public DbSet<Municipio> Municipio { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=JC-PC;Initial Catalog=ProyectoEjemplo;Integrated Security=True");
+        }
+    }
+}
---
+++ DeptosES.PruebasUnitarias/MunicipioDALTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeptosES.AccesoADatos;
+using DeptosES.EntidadesDeNegocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeptosES.AccesoADatos.Tests
+{
+    [TestClass()]
+    public class MunicipioDALTests
+    {
+        private static Municipio municipioInicial = new Municipio { Id = 2, IdDepartamento = 1 };
+
+        [TestMethod()]
+        public async Task T1CrearAsyncTest()
+        {
+            var municipio = new Municipio();
+            municipio.IdDepartamento = municipioInicial.IdDepartamento;
+            municipio.Nombre = "El Congo";
+            municipio.Descripcion = "Su atractivo principal es el lago de Coatepeque";
+            municipio.Imagen = "http://online.salvadoreantours.com/wp-content/uploads/2018/08/Coatepeque-Lake.jpeg";
+            int result = await MunicipioDAL.CrearAsync(municipio);
+            Assert.AreNotEqual(0, result);
+            municipioInicial.Id = municipio.Id;
+        }
+
+        [TestMethod()]
+        public async Task T2ModificarAsyncTest()
+        {
+            var municipio = new Municipio();
+            municipio.Id = municipioInicial.Id;
+            municipio.IdDepartamento = municipioInicial.IdDepartamento;
+            municipio.Nombre = "Coatepeque";
+            municipio.Descripcion = "Su atractivo principal es el lago de Coatepeque";
+            municipio.Imagen = "http://online.salvadoreantours.com/wp-content/uploads/2018/08/Coatepeque-Lake.jpeg";
+            int result = await MunicipioDAL.CrearAsync(municipio);
+            Assert.AreNotEqual(0, result);
+            municipioInicial.Id = municipio.Id;
+        }        
+
+        [TestMethod()]
+        public async Task T3ObtenerPorIdAsyncTest()
+        {
+            var municipio = new Municipio();
+            municipio.Id = municipioInicial.Id;
+            var resultMunicipio = await MunicipioDAL.ObtenerPorIdAsync(municipio);
+            Assert.AreEqual(municipio.Id, resultMunicipio.Id);
+        }
+
+        [TestMethod()]
+        public async Task T4ObtenerTodosAsyncTest()
+        {
+            var resultMunicipios = await MunicipioDAL.ObtenerTodosAsync();
+            Assert.AreNotEqual(0, resultMunicipios.Count);
+        }
+
+        [TestMethod()]
+        public async Task T5BuscarAsyncTest()
+        {
+            var municipio = new Municipio();
+            municipio.IdDepartamento = municipioInicial.IdDepartamento;
+            municipio.Nombre = "A";
+            municipio.Top_Aux = 10;
+            var resultMunicipios = await MunicipioDAL.BuscarAsync(municipio);
+            Assert.AreNotEqual(0, resultMunicipios.Count);
+        }
+
+        [TestMethod()]
+        public async Task T6BuscarIncluirDepartamentosAsyncTest()
+        {
+            var municipio = new Municipio();
+            municipio.IdDepartamento = municipioInicial.IdDepartamento;
+            municipio.Nombre = "A";
+            municipio.Top_Aux = 10;
+            var resultMunicipios = await MunicipioDAL.BuscarIncluirDepartamentosAsync(municipio);
+            Assert.AreNotEqual(0, resultMunicipios.Count);
+        }
+
+        [TestMethod()]
+        public async Task T7EliminarAsyncTest()
+        {
+            var municipio = new Municipio();
+            municipio.Id = municipioInicial.Id;
+            int result = await MunicipioDAL.EliminarAsync(municipio);
+            Assert.AreNotEqual(0, result);
+        }
+    }
+}
